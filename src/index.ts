e93350821e9220cd67d12e33b4b117138577ccfd
Y-Bingo <y_bingo@163.com>
2019-06-25 18:17:06 +0800

    处理请求body数据
    修改对象判断 isObject =》 isPlainObject
    
    补上忘记添加的data数据处理请求

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
-import { AxiosRequestConfig } from './types'
 import xhr from './xhr'
+import { AxiosRequestConfig } from './types'
 import { buildURL } from './helpers/url'
+import { transformRequest } from './helpers/data'
 
 function axios(config: AxiosRequestConfig): void {
   // TODO
@@ -11,12 +12,18 @@ function axios(config: AxiosRequestConfig): void {
 // 请求参数预处理
 function processConfig(config: AxiosRequestConfig): void {
   config.url = transformURL(config)
+  config.data = transformRequestData(config)
 }
 
 // url处理
 function transformURL(config: AxiosRequestConfig): string {
   const { url, params } = config
   return buildURL(url, params)
+}
+
+// body数据处理
+function transformRequestData(config: AxiosRequestConfig): any {
+  return transformRequest(config.data)
 }
 
 export default axios
