6a9e96e6706f4e334bff38ce860d25247a152e6d
Y-Bingo <y_bingo@163.com>
2019-06-25 16:50:30 +0800

    处理请求url参数

---
+++ src/helpers/url.ts
@@ -0,0 +1,123 @@
+import { isObject, isDate } from './utils'
+
+// 进行URI编码 支持特殊字符
+/**
+ * 特殊字符支持
+ * 对于字符 @、:、$、,、、[、]，我们是允许出现在 url 中的，不希望被 encode
+ * params: {
+ *     foo: '@:$, '
+ * }
+ * 最终请求的 url 是 /base/get?foo=@:$+，注意，我们会把空格 转换成 +
+ */
+function encode(val: string): string {
+  return encodeURIComponent(val)
+    .replace(/%40/g, '@')
+    .replace(/%3A/g, ':')
+    .replace(/%24/g, '$')
+    .replace(/%2C/gi, ',')
+    .replace(/%20/g, '+')
+    .replace(/%5B/gi, '[')
+    .replace(/%5D/gi, ']')
+}
+/**
+ *  axios({
+ *      method: 'get',
+ *      url: '/base/get',
+ *      params: {
+ *      }
+ * })
+ * 根据params来构建url
+ */
+export function buildURL(url: string, params?: any): string {
+  if (!params) {
+    return url
+  }
+
+  const parts: string[] = []
+
+  Object.keys(params).forEach(key => {
+    const val = params[key]
+
+    /**
+     * 空值忽略
+     * 对于值为 null 或者 undefined 的属性，我们是不会添加到 url 参数中的
+     * params: {
+     *     foo: 'bar',
+     *     baz: null
+     * }
+     * 最终请求的 url 是 /base/get?foo=bar
+     */
+    if (val === null || typeof val === 'undefined') {
+      return
+    }
+
+    let values = []
+    /**
+     * 参数值为数组
+     * params: {
+     *     foo: ['bar', 'baz']
+     * }
+     * 最终请求的 url 是 /base/get?foo[]=bar&foo[]=baz'
+     */
+    if (Array.isArray(val)) {
+      values = val
+      key += '[]'
+    } else {
+      values = [val]
+    }
+
+    values.forEach(val => {
+      if (isDate(val)) {
+        /**
+         * 参数值为Date类型
+         * params: {
+         *     foo: new Date()
+         * }
+         * 最终请求的 url 是 /base/get?date=2019-04-01T05:55:39.030Z，date 后面拼接的是 date.toISOString() 的结果
+         */
+        val = val.toISOString()
+      } else if (isObject(val)) {
+        /**
+         * 参数值为对象
+         * params: {
+         *     foo: {
+         *          bar: 'baz'
+         *     }
+         * }
+         * 最终请求的 url 是 /base/get?foo=%7B%22bar%22:%22baz%22%7D，foo 后面拼接的是 {"bar":"baz"} encode 后的结果
+         */
+        val = JSON.stringify(val)
+      }
+      parts.push(`${encode(key)}=${encode(val)}`)
+    })
+  })
+
+  // 拼接
+  let serializedParams = parts.join('&')
+
+  if (serializedParams) {
+    /**
+     * 丢弃 url 中的哈希标记
+     * url: '/base/get#hash',
+     * params: {
+     *     foo: 'bar'
+     * }
+     * 最终请求的 url 是 /base/get?foo=bar
+     */
+    const markIndex = url.indexOf('#')
+    if (markIndex !== -1) {
+      url = url.slice(0, markIndex)
+    }
+    /**
+     * 保留 url 中已存在的参数
+     * url: '/base/get?foo=bar',
+     * params: {
+     *     bar: 'baz'
+     * }
+     * 最终请求的 url 是 /base/get?foo=bar&bar=baz
+     */
+    url += (url.indexOf('?') === -1 ? '?' : '&') + serializedParams
+  }
+
+  return url
+}
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,22 @@
-import { AxiosRequestConfig } from "./types";
-import xhr from "./xhr";
+import { AxiosRequestConfig } from './types'
+import xhr from './xhr'
+import { buildURL } from './helpers/url'
 
-function axios ( config: AxiosRequestConfig ): void 
-{
-    // TODO
-    xhr( config );
+function axios(config: AxiosRequestConfig): void {
+  // TODO
+  processConfig(config)
+  xhr(config)
 }
 
+// 请求参数预处理
+function processConfig(config: AxiosRequestConfig): void {
+  config.url = transformURL(config)
+}
 
-export default axios;
+// url处理
+function transformURL(config: AxiosRequestConfig): string {
+  const { url, params } = config
+  return buildURL(url, params)
+}
+
+export default axios
