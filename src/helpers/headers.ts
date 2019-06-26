1de2fad7404fcfd3bfecd393e4c6390c9432d083
Y-Bingo <y_bingo@163.com>
2019-06-26 10:01:51 +0800

    处理请求header

---
+++ src/helpers/headers.ts
@@ -0,0 +1,36 @@
+import { isPlainObject } from './utils'
+
+/**
+ *
+ * 规范化 header 属性名
+ * @param headers
+ * @param normalizeName
+ */
+function normalizeHeaderName(headers: any, normalizeName: string): void {
+  if (!headers) {
+    return
+  }
+
+  Object.keys(headers).forEach(name => {
+    if (name !== normalizeName && name.toUpperCase() === normalizeName.toUpperCase()) {
+      headers[normalizeName] = headers[name]
+      delete headers[name]
+    }
+  })
+}
+
+/**
+ * 处理headers
+ * @param headers
+ * @param data
+ */
+export function processHeaders(headers: any, data: any): any {
+  normalizeHeaderName(headers, 'Content-Type')
+  if (isPlainObject(data)) {
+    if (headers && !headers['Content-Type']) {
+      headers['Content-type'] = 'application/json;charset=utf-8'
+    }
+  }
+
+  return headers
+}
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,14 +1,20 @@
 export type Method =
-    "get" | "GET" |
-    "delete" | "DELETE" |
-    "head" | "HEAD" |
-    "post" | "POST" |
-    "options" | "OPTIONS" |
-    "put" | "PUT";
-export interface AxiosRequestConfig
-{
-    url: string;
-    method?: Method;
-    data?: any;
-    params?: any;
+  | 'get'
+  | 'GET'
+  | 'delete'
+  | 'DELETE'
+  | 'head'
+  | 'HEAD'
+  | 'post'
+  | 'POST'
+  | 'options'
+  | 'OPTIONS'
+  | 'put'
+  | 'PUT'
+export interface AxiosRequestConfig {
+  url: string
+  method?: Method
+  data?: any
+  headers?: any
+  params?: any
 }
