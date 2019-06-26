4b9e651ec3e90b98117f3b23dbdbceb84af5bbf1
Y-Bingo <y_bingo@163.com>
2019-06-26 16:12:59 +0800

    错误处理 1. 链接错误 2. 超时错误 3. 响应错误

--- src/xhr.ts
+++ src/xhr.ts
@@ -2,8 +2,8 @@ import { AxiosRequestConfig, AxiosPromise, AxiosResponse } from './types'
 import { parseHeaders } from './helpers/headers'
 
 export default function xhr(config: AxiosRequestConfig): AxiosPromise {
-  return new Promise(resolve => {
-    const { data = null, url, method = 'get', headers, responseType } = config
+  return new Promise((resolve, reject) => {
+    const { data = null, method = 'get', url, headers, responseType, timeout } = config
 
     const request = new XMLHttpRequest()
     // 打开一个连接
@@ -21,11 +21,24 @@ export default function xhr(config: AxiosRequestConfig): AxiosPromise {
     if (responseType) {
       request.responseType = responseType
     }
+    // 设置超时时间
+    if (timeout) {
+      request.timeout = timeout
+    }
 
+    // 监听网络错误
+    request.onerror = function handleError() {
+      reject(new Error('##NETWORK 网络连接错误'))
+    }
+    // 监听超时
+    request.ontimeout = function handlerTimeout() {
+      reject(new Error(` ##timeout 网络连接超时: ${this.timeout}`))
+    }
     // 监听请求状态
-    request.onreadystatechange = function() {
+    request.onreadystatechange = function handleLoad() {
       // 判断请求状态
       if (request.readyState !== 4) return
+      if (request.status === 0) return
       // 获取响应数据
       const responseData = request.responseType === 'text' ? request.responseText : request.response
       // 获取响应头
@@ -39,9 +52,27 @@ export default function xhr(config: AxiosRequestConfig): AxiosPromise {
         config,
         request
       }
-      resolve(response)
+      handlerResponse(response, resolve, reject)
     }
     // 发送
     request.send(data)
   })
+}
+
+/**
+ * 响应处理 处理非200状态码
+ * 对于一个正常的请求，往往会返回 200-300 之间的 HTTP 状态码，
+ * 对于不在这个区间的状态码，我们也把它们认为是一种错误的情况做处理
+ * @param res
+ */
+function handlerResponse<T extends AxiosResponse>(
+  res: T,
+  resolve: (value?: T | PromiseLike<T>) => void,
+  reject: (reason?: any) => void
+): void {
+  if (res.status >= 200 && res.status < 300) {
+    resolve(res)
+  } else {
+    reject(new Error(`##STATUS 请求失败 状态码: ${res.status}`))
+  }
 }
