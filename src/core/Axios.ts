3e458bae113393f3d06a7ac38e0ad31429ce9bcc
Y-Bingo <y_bingo@163.com>
2019-06-27 10:33:41 +0800

    axios函数重载

--- src/core/Axios.ts
+++ src/core/Axios.ts
@@ -3,7 +3,13 @@ import dispatchRequest from './dispatchRequest'
 
 export class Axios {
   // 基础请求方法
-  request(config: AxiosRequestConfig): AxiosPromise {
+  request(url: any, config?: any): AxiosPromise {
+    if (typeof url === 'string') {
+      if (!config) config = {}
+      config.url = url
+    } else {
+      config = url
+    }
     return dispatchRequest(config)
   }
 
